f7190f94744928a0ea2d62f5a25dd7e30248fec5
Joaquim <juakacc@gmail.com>
2020-05-10 11:26:19 -0300

    Initializing project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Typography, Link } from "@material-ui/core";
+import useStyles from "./styles";
+
+export default function Copyright() {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link color="inherit" href="https://olivedos.pb.gov.br">
+          Viagens PMO
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    </footer>
+  );
+}
